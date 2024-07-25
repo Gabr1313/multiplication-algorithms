7742f6f0096a830b4b11a8c5a374c1464be222ad
Gabr1313 <gabriele.baietta03@gmail.com>
2024-07-25 15:59:03 +0200

    better testing sh file

--- test.sh
+++ test.sh
@@ -2,16 +2,15 @@
 
 ./gen $1 $1 >in.gen.txt
 
-./karatsuba <in.gen.txt >out.karatsuba.txt 2> /tmp/time.karatsuba.txt &
-pid_karatsuba=$!
+time_karatsuba=$( { ./karatsuba <in.gen.txt 2>&1 1>out.karatsuba.txt; } &
+                   pid_karatsuba=$! )
 
-./naif      <in.gen.txt >out.naif.txt      2> /tmp/time.naif.txt      &
-pid_naif=$!
+time_naif=$(      { ./naif      <in.gen.txt 2>&1 1>out.naif.txt;      } &
+                   pid_naif=$!      )
 
 wait $pid_karatsuba
-echo "Time karatsuba: $(cat /tmp/time.karatsuba.txt)"
-
+echo "Time karatsuba: " $time_karatsuba
 wait $pid_naif
-echo "Time naif:      $(cat /tmp/time.naif.txt     )"
+echo "Time naif:      " $time_naif
 
 diff -q out.naif.txt out.karatsuba.txt
