b27b65555fbee53e3ace11a353562283147a84c3
Gabr1313 <gabriele.baietta03@gmail.com>
2024-07-25 16:09:08 +0200

    rollback of test.sh

--- test.sh
+++ test.sh
@@ -2,15 +2,16 @@
 
 ./gen $1 $1 >in.gen.txt
 
-time_karatsuba=$( { ./karatsuba <in.gen.txt 2>&1 1>out.karatsuba.txt; } &
-                   pid_karatsuba=$! )
+./karatsuba <in.gen.txt >out.karatsuba.txt 2> time.karatsuba.txt &
+pid_karatsuba=$!
 
-time_naif=$(      { ./naif      <in.gen.txt 2>&1 1>out.naif.txt;      } &
-                   pid_naif=$!      )
+./naif      <in.gen.txt >out.naif.txt      2> time.naif.txt      &
+pid_naif=$!
 
 wait $pid_karatsuba
-echo "Time karatsuba: " $time_karatsuba
+echo "Time karatsuba: $(cat time.karatsuba.txt)"
+
 wait $pid_naif
-echo "Time naif:      " $time_naif
+echo "Time naif:      $(cat time.naif.txt     )"
 
 diff -q out.naif.txt out.karatsuba.txt
